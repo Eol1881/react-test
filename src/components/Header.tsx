34bb6066bc0c769dc2d4f136b6107ea4f8991754
Eol1881 <02f1881@gmail.com>
2023-10-01 12:53:29 +0300

    refactor: class -> functional components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
-import { Component, ReactNode } from 'react';
+import { ChangeEvent, useState } from 'react';
 
-export class Header extends Component {
-  render(): ReactNode {
-    const savedSearchTerm = localStorage.getItem('search-term');
+export function Header() {
+  const [searchTerm, setSearchTerm] = useState(localStorage.getItem('search-term') || '');
 
-    return (
-      <div className="header card">
-        <input
-          className="input"
-          type="text"
-          defaultValue={savedSearchTerm ? savedSearchTerm : ''}
-          onChange={(changeEvent) => {
-            localStorage.setItem('search-term', changeEvent.currentTarget.value);
-          }}
-        />
-        <button className="search-button">Search</button>
-      </div>
-    );
-  }
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    localStorage.setItem('search-term', value);
+  };
+
+  return (
+    <div className="header card">
+      <input className="input" type="text" defaultValue={searchTerm} onChange={handleInputChange} />
+      <button className="search-button">Search</button>
+    </div>
+  );
 }
+
+// export function Header() {
+//   const savedSearchTerm = localStorage.getItem('search-term');
+
+//   return (
+//     <div className="header card">
+//       <input
+//         className="input"
+//         type="text"
+//         defaultValue={savedSearchTerm ? savedSearchTerm : ''}
+//         onChange={(changeEvent) => {
+//           localStorage.setItem('search-term', changeEvent.currentTarget.value);
+//         }}
+//       />
+//       <button className="search-button">Search</button>
+//     </div>
+//   );
+// }
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,38 +1,72 @@
-import { Component, ReactNode } from 'react';
+import { useState, useEffect } from 'react';
 
 interface Pokemon {
   url: number;
   name: string;
 }
 
-export class Result extends Component {
-  state = {
-    pokemons: [] as Pokemon[],
-  };
+export function Result() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-  async fetchPokemons() {
+  const fetchPokemons = async () => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon');
     const pokemonsData = await response.json();
     console.log(pokemonsData);
-    this.setState({ pokemons: pokemonsData.results });
-  }
+    setPokemons(pokemonsData.results);
+  };
 
-  async componentDidMount() {
-    await this.fetchPokemons();
-  }
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
 
-  render(): ReactNode {
-    const { pokemons } = this.state;
-    return (
-      <div className="result card">
-        <ul>
-          {pokemons.map((pokemon) => (
-            <li key={pokemon.name} className="pokemon">
-              {pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1)}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="result card">
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.name} className="pokemon">
+            {pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+// import { Component, ReactNode } from 'react';
+
+// interface Pokemon {
+//   url: number;
+//   name: string;
+// }
+
+// export class Result extends Component {
+//   state = {
+//     pokemons: [] as Pokemon[],
+//   };
+
+//   async fetchPokemons() {
+//     const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+//     const pokemonsData = await response.json();
+//     console.log(pokemonsData);
+//     this.setState({ pokemons: pokemonsData.results });
+//   }
+
+//   async componentDidMount() {
+//     await this.fetchPokemons();
+//   }
+
+//   render(): ReactNode {
+//     const { pokemons } = this.state;
+//     return (
+//       <div className="result card">
+//         <ul>
+//           {pokemons.map((pokemon) => (
+//             <li key={pokemon.name} className="pokemon">
+//               {pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1)}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
